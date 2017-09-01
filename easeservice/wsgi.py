06e5a8c93eed9f44f70e81a246c461482f361a22
root <root@ip-172-31-28-174.ap-south-1.compute.internal>
2017-09-01 09:14:54 +0000

    pending server changes

--- easeservice/wsgi.py
+++ easeservice/wsgi.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 WSGI config for easeservice project.
 
@@ -19,7 +20,7 @@ os.environ['PYTHON_EGG_CACHE'] = '/tmp/egg'
 if server_config and server_config.__contains__("Development"):
     sys.path.append("/host/Ease/easeserver/easeservice")
 else:
-    sys.path.append("/var/www/html/easeserver/easeservice")
+    sys.path.append("/var/ease/dealer-webapp")
 sys.path.append("/host/Ease/easeserver/easeservice")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "easeservice.settings")
 application = get_wsgi_application()
