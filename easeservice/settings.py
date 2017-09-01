bfb2480fdce4072b934e6843b86733be472adbda
Sanjay <sanjayortiz@gmail.com>
2017-09-01 15:25:38 +0530

    update

--- easeservice/settings.py
+++ easeservice/settings.py
@@ -91,7 +91,7 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
-            ]
+            ],
             'debug': DEBUG,
         },
     },
