6955c7715f01ec8c418fdd5ae3e9f89061351ee2
Sanjay <sanjayortiz@gmail.com>
2017-09-03 17:17:29 +0530

    env update

--- manage.py
+++ manage.py
@@ -4,7 +4,10 @@ import sys
 
 if __name__ == "__main__":
 
-    if os.environ['ENV_TYPE'] == "00":
+    if (
+    	'ENV_TYPE' in os.environ and
+    	os.environ['ENV_TYPE'] == "00"
+    	):
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "easeservice.local_settings")
     else:
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "easeservice.settings")
